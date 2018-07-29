83ae478fc1070d5ba49bf3ce5e644368aa93157d
Jane Justiz <janejustiz@gmail.com>
2018-07-29 05:13:29 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,14 @@
 class Backer
   attr_accessor :name
 
-  @@backed_projects = []
+
 
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
 
   def back_project(backer)
-    @@backed_projects << backer
+    @backed_projects << backer
   end
 end
